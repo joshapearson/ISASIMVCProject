0d6dbdb7d6aebca239576f0c6aa75e9930e84bc3
joshua allen pearson <joshapearson@gmail.com>
2017-06-02 17:58:26 -0500

    First Commit

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ISASIMVCProject.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ISASIMVCProject.Startup))]
+namespace ISASIMVCProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
